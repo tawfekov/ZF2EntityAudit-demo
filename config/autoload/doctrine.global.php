9f8af0c1cfea15dc0bc5d0d354cbc5c9cfde9c44
Tawfek Daghistani <tawfekov@hotmail.com>
2013-04-02 13:03:48 +0300

    0.2-demo application

--- config/autoload/doctrine.global.php
+++ config/autoload/doctrine.global.php
@@ -9,7 +9,7 @@ return array(
                     'host'     => 'localhost',
                     'port'     => '3306',
                     'user'     => 'root',
-                    'password' => '',
+                    'password' => 'root',
                     'dbname'   => 'entityaudit',
                 )
             )
--- config/autoload/zf2entityaudit.global.php
+++ config/autoload/zf2entityaudit.global.php
@@ -5,7 +5,10 @@ return array(
         'entities' => array(
             'Application\Entity\Type'
         ),
-
+        'ui' => array(
+            'datetime.format' => 'r',
+            'page.limit'    => '20'
+        ),
         'zfcuser.integration' => true,
     ),
 );
